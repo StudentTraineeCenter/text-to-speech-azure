c16413f9a7f56a6eb0bf73fe59d4f96dea3561cd
Michal Vaniš <zirixcz@gmail.com>
2022-05-16 20:29:37 +0200

    adding functionality

--- src/TextToSpeech/SpeechSynthesis.js
+++ src/TextToSpeech/SpeechSynthesis.js
@@ -1,27 +1,28 @@
 import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+import {SpeechSynthesisOutputFormat} from "microsoft-cognitiveservices-speech-sdk";
 import { useEffect } from "react";
 
 const SpeechSynthesis = (props) => {
-    let language = "en-US-JennyNeural";
-    if (props.language === "English") {
-        language = "en-US-JennyNeural";
+    // jazyk
+    let language = "en-US";
+    if (props.language === "Czech") {
+        language = "cs-CZ";
     }
-    else if (props.language === "Czech") {
-        language = "cs-CZ-VlastaNeural";
-    }
+    // řeč
+    let voice = (language === "en-US") ? "en-CA-ClaraNeural" : "cs-CZ-AntoninNeural";
+
     useEffect(() => {
         const speechConfig = sdk.SpeechConfig
             .fromSubscription(process.env.REACT_APP_AZUREKEY, "northeurope");
-        speechConfig.speechSynthesisVoiceName = language;
-
+        // kvalita
+        speechConfig.speechSynthesisOutputFormat = SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm;
         let synthesizer = new sdk.SpeechSynthesizer(speechConfig);
-        synthesizer.speakTextAsync(
-            props.text,
+        synthesizer.speakSsmlAsync(`<speak version="1.0" xml:lang="${language}"><voice name="${voice}"><prosody rate="10%" pitch="0%">${props.text}</prosody></voice></speak>`,
             (result) => {
-                synthesizer.close();
-                return result.audioData;
-            }
-        );
+            synthesizer.close();
+            console.log(result.audioData);
+            return result.audioData;
+       })
     }, [props.text])
 }
 export default SpeechSynthesis;
